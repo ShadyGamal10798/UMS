e37820694e24be6824c6a28dc35a3f0a5c86cae4
ShadyGamal10798 <shade_EN30894828@f-eng.tanta.edu.eg>
2023-08-06 08:48:22 +0300

    UploadProject

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/dist/vuetify.min.css';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+const app = createApp(App).use(vuetify)
+
+
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
